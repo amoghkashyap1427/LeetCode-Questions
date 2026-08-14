839c5fbbbccffc4146b6cb76ff295182b1aec041
Amogh Kashyap <amoghkashyap1427@gmail.com>
2026-08-14 11:25:48 +0530

    Time: 0 ms (100.00%) | Memory: 43.4 MB (99.98%) - LeetSync

--- 4216-weighted-word-mapping/weighted-word-mapping.cpp
+++ 4216-weighted-word-mapping/weighted-word-mapping.cpp
@@ -1,29 +1,15 @@
 class Solution {
 public:
     string mapWordWeights(vector<string>& words, vector<int>& weights) {
-        unordered_map<int, char>mapOfReverse;
-        unordered_map<int, char>mapOfAlpha;
-        int c=0;
-        string st="";
-        for(char ch='z'; ch>='a'; ch--){
-            mapOfReverse[c++]=ch;
-        }
-        c=0;
-        for(char ch='a'; ch<='z'; ch++){
-            mapOfAlpha[ch]=c;
-            c++;
-        }
-
-        for(auto& s : words){
-            int sum=0;
-            for(auto& chara : s){
-                sum+=weights[mapOfAlpha[chara]];
+        string result = "";
+        for (auto& s : words) {
+            int sum = 0;
+            for (char ch : s) {
+                sum += weights[ch - 'a'];
             }
-
-            int rem = sum%26;
-            st+=mapOfReverse[rem];
+            int rem = sum % 26;
+            result += ('z' - rem); // reverse mapping: 0->'z', 1->'y', ...
         }
-
-        return st;
+        return result;
     }
 };
