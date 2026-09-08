4e0527f142b8b6acc4aa44e30256d7fc01813018
Amogh Kashyap <amoghkashyap1427@gmail.com>
2026-09-08 10:35:02 +0530

    Time: 0 ms (100.00%) | Memory: 8.6 MB (51.38%) - LeetSync

--- 4245-count-commas-in-range/count-commas-in-range.cpp
+++ 4245-count-commas-in-range/count-commas-in-range.cpp
@@ -3,13 +3,11 @@ public:
     int countCommas(int n) {
         long long ans = 0;
         long long power = 1000;
-        int commas = 1;
 
         while (power <= n) {
             ans += n - power + 1;
 
             power *= 1000;
-            commas++;
         }
 
         return ans;
